9264af530851b1daa71a333f79dddc2ec7b96f23
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-11-14 21:10:04 -0500

    edit project detailes

--- src/GroceryList.js
+++ src/GroceryList.js
@@ -30,40 +30,48 @@ export class GroceryList extends Component {
     const li = event.target;
     li.classList.toggle("crossed");
   }
+  onFormSumbit(e) {
+    e.preventDefault();
+  }
   render() {
     return (
       <div>
-        <div className="container">
-          <input
-            type="text"
-            placeholder="What whould you like to buy?"
-            onChange={(e) => {
-              this.onChangeEvent(e.target.value);
-            }}
-            value={this.state.userInput}
-          />
-        </div>
-        <div className="container">
-          <button
-            onClick={() => this.addItem(this.state.userInput)}
-            className="btn-add btn"
-          >
-            Add
-          </button>
-        </div>
-        <ul>
-          {this.state.grosetyList.map((item, index) => (
-            <li onClick={this.crossedWord} key={index}>
-              <img src={check} width="40px" alt="check mark" />
-              {item}
-            </li>
-          ))}
-        </ul>
-        <div className="container">
-          <button onClick={() => this.deleteItem()} className="btn-delete btn">
-            Delete
-          </button>
-        </div>
+        <form onSubmit={this.onFormSumbit}>
+          <div className="container">
+            <input
+              type="text"
+              placeholder="What whould you like to buy?"
+              onChange={(e) => {
+                this.onChangeEvent(e.target.value);
+              }}
+              value={this.state.userInput}
+            />
+          </div>
+          <div className="container">
+            <button
+              onClick={() => this.addItem(this.state.userInput)}
+              className="btn-add btn"
+            >
+              Add
+            </button>
+          </div>
+          <ul>
+            {this.state.grosetyList.map((item, index) => (
+              <li onClick={this.crossedWord} key={index}>
+                <img src={check} width="40px" alt="check mark" />
+                {item}
+              </li>
+            ))}
+          </ul>
+          <div className="container">
+            <button
+              onClick={() => this.deleteItem()}
+              className="btn-delete btn"
+            >
+              Delete
+            </button>
+          </div>
+        </form>
       </div>
     );
   }
