a0709bb85bc5606770165768a679ee7d6ed699ae
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-11-14 19:36:57 -0500

    add button ADD

--- src/GroceryList.js
+++ src/GroceryList.js
@@ -1,23 +1,40 @@
 import { Component } from "react";
 
-export class GroceryList extends Component{
-    state = {
-        userInput: "",
-        grosetyList: []
-    }
+export class GroceryList extends Component {
+  state = {
+    userInput: "",
+    grosetyList: [],
+  };
 
-    onChangeEvent(e){
-        this.setState({userInput: e})
-        console.log(e)
-    }
-    render(){
-        return(
-            <div>
-                <input
-                type="text" placeholder="What whould you like to buy?"
-                onChange={(e) =>{this.onChangeEvent(e.target.value)}}
-                value={this.state.userInput} />
-            </div>
-        )
-    }
+  onChangeEvent(e) {
+    this.setState({ userInput: e });
+    console.log(e);
+  }
+  addItem(input) {
+    let listArray = this.state.grosetyList;
+    listArray.push(input);
+    this.setState({ grosetyList: listArray, userInput: "" });
+    console.log(listArray);
+  }
+  render() {
+    return (
+      <div>
+        <div>
+          <input
+            type="text"
+            placeholder="What whould you like to buy?"
+            onChange={(e) => {
+              this.onChangeEvent(e.target.value);
+            }}
+            value={this.state.userInput}
+          />
+        </div>
+        <div>
+          <button onClick={() => this.addItem(this.state.userInput)}>
+            Add
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
